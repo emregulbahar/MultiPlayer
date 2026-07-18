f6eb28ba6a89da296e9e7b761ffbc6e2cd181ac4
emregulbahar <yunusemregulbaharr@gmail.com>
2026-07-18 10:52:58 +0300

    v0.1 Assets Added & Client Created

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System;
+using Unity.Netcode;
+
+public class GameManager : NetworkBehaviour
+{
+    [SerializeField] private MultiplayerUI _multiPlayerUI;
+
+
+    private void Start() {
+        if(_multiPlayerUI != null)
+        {
+            _multiPlayerUI.OnStartHost += StartHost;
+            _multiPlayerUI.OnStartClient += StartClient;
+            _multiPlayerUI.OnDiconnectClient += DisconnectClient;
+        }
+    }
+
+    private void StartHost()
+    {
+        _multiPlayerUI.DisableButtons();
+        NetworkManager.StartHost();
+    }
+
+    private void StartClient()
+    {
+        _multiPlayerUI.DisableButtons();
+        NetworkManager.StartClient();
+    }
+
+    private void DisconnectClient()
+    {
+        _multiPlayerUI.DisableButtons();
+        NetworkManager.Shutdown();
+    }
+}
